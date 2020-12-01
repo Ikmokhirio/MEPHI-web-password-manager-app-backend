ee6784cd75c0fdc726919d70016462f09bdf530b
ikmokhirio <ikmokhirio@mail.ru>
2020-12-01 11:43:37 +0300

    Fixed user model

--- migrations/20201123062413-create-user.js
+++ migrations/20201123062413-create-user.js
@@ -24,6 +24,10 @@ module.exports = {
                 type: Sequelize.STRING,
                 allowNull: false
             },
+            master_password: {
+              type: Sequelize.STRING,
+              allowNull: false
+            },
             createdAt: {
                 allowNull: false,
                 type: Sequelize.DATE
